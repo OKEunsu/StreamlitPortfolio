fbcbee3621c86ca8cec3672a19cd811124224ba8
Eunsu Ok <77106134+OKEunsu@users.noreply.github.com>
2024-12-14 00:25:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,6 @@ hide_streamlit_style = """
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 header {visibility: hidden;}
-._profileContainer_gzau3_53:hover {
-    display: none !important;
-    pointer-events: none !important;
-}
-
-._profileContainer_gzau3_53 {
-    display: none !important;
-}
 </style>
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
