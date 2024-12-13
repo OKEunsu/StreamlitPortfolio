d5b74a1860c1ce0de71f5564cda6d73d1e0ac889
Eunsu Ok <77106134+OKEunsu@users.noreply.github.com>
2024-12-13 23:42:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 import streamlit as st
 import yfinance as yf
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
 
 # Function to delete a stock from the list
 def delete_stock(index):
